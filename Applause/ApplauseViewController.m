89c81b29fe00a04fbaa461f4d84e0c88dc175f62
grandbora <bora.tunca@yahoo.com>
2013-06-08 23:04:34 +0200

    add configs

--- Applause/ApplauseViewController.m
+++ Applause/ApplauseViewController.m
@@ -13,17 +13,20 @@
 @end
 
 @implementation ApplauseViewController
-@synthesize playerList, applause1Data,lastTapTime;
+@synthesize playerList, levelConfigList, applause1Data;
 @synthesize tapGestureLevel1, tapGestureLevel2;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self initializeApplauseData];
-    
-    playerList = [[NSMutableArray alloc] init]; // I don't know what I am doing!
-    [tapGestureLevel1 requireGestureRecognizerToFail:tapGestureLevel2];
 
+    playerList = [[NSMutableArray alloc] init]; // I don't know what I am doing!
+    levelConfigList = [[NSMutableArray alloc] init]; // I don't know what I am doing!
+    
+    [self initializeLevelConfigData];
+    [self initializeApplauseData];
+
+    [tapGestureLevel1 requireGestureRecognizerToFail:tapGestureLevel2];
 
     //write ready GO
 }
@@ -46,68 +49,50 @@
     [self changeToStartColor];
 }
 
-- (IBAction)tapLevel1Catched:(id)sender {
-    
+- (IBAction)tapLevel1Catched:(id)sender
+{
     [self changeToStartColor];
-    
-    if (false == [self isTapIntervalReached])
-    {
-        [self playSoundLevel1];
-    }
+    [self playLevelSound:0];
 }
 
-- (IBAction)tapLevel2Catched:(id)sender {
-    
+- (IBAction)tapLevel2Catched:(id)sender
+{    
     [self changeToStartColor];
-    
-    if (false == [self isTapIntervalReached])
-    {
-        [self playSoundLevel2];
-    }
+    [self playLevelSound:1];
 }
 
 //------PRI
-
-- (BOOL) isTapIntervalReached
+- (void) playLevelSound:(int)level
 {
+    if (false == [self isMinIntervalReached:level])
+    {
+        [self playSound:level];
+    }
+}
+
+//------UTIL
+- (BOOL) isMinIntervalReached:(int)level
+{
+    NSMutableDictionary *config = [levelConfigList objectAtIndex:level];
+    double minInterval = [[config objectForKey:@"minInterval"] doubleValue];
+    double lastTapTime = [[config objectForKey:@"lastRecognitionTime"] doubleValue];
     double currentTapTime = [[NSDate date] timeIntervalSince1970];
-    double interval = currentTapTime - lastTapTime;
     
-    if (interval > 0.3) {
-        lastTapTime = currentTapTime;
+    if (currentTapTime - lastTapTime > minInterval) {
+        [config setObject: [NSString stringWithFormat:@"%f", currentTapTime] forKey:@"lastRecognitionTime"];
         return false;
     }
 
-//    NSLog(@"interval :  %f", interval);
     return true;
 }
 
-- (void) playSound
+- (void) playSound:(int)level
 {    
     NSError *error;
     AVAudioPlayer *player = [[AVAudioPlayer alloc] initWithData:applause1Data error:&error];
     [player play];
     [playerList addObject: player];
-    //NSLog(@"Number of players :  %d", [playerList count]);
-    //BDNF clear old players
-}
-
-- (void) initializeApplauseData
-{
-    //66234588
-    //96004778
-    //79675240
-    NSString *stream_url = @"https://api.soundcloud.com/tracks/79675240/stream?client_id=fa1fd2df5a17a560f8456aed4016160a"; //remove hardcore
-    
-    [SCRequest performMethod:SCRequestMethodGET
-                  onResource:[NSURL URLWithString:stream_url]
-             usingParameters:nil
-                 withAccount:nil
-      sendingProgressHandler:nil
-             responseHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
-                 applause1Data = data;
-                 NSLog(@"Applause Data LOADED");
-             }];
+    //NSLog(@"Number of players :  %d", [playerList count]); //BDNF clear old players
 }
 
 - (void) changeToStartColor
@@ -118,6 +103,51 @@
 - (void) changeToTapColor
 {
     self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void) initializeLevelConfigData
+{
+    NSMutableDictionary *level1Config = [[NSMutableDictionary alloc] init];
+    [level1Config setObject:@"79675240" forKey:@"trackId"];
+    [level1Config setObject:@"3" forKey:@"duration"];
+    [level1Config setObject:@"1" forKey:@"minInterval"];
+    [level1Config setObject:@"0" forKey:@"lastRecognitionTime"];
+    [levelConfigList addObject: level1Config];
+    
+    NSMutableDictionary *level2Config = [[NSMutableDictionary alloc] init];
+    [level2Config setObject:@"96004778" forKey:@"trackId"];
+    [level2Config setObject:@"4" forKey:@"duration"];
+    [level2Config setObject:@"3" forKey:@"minInterval"];
+    [level2Config setObject:@"0" forKey:@"lastRecognitionTime"];
+    [levelConfigList addObject: level2Config];
+    
+    //66234588
+    //96004778
+    //79675240
+}
+
+- (void) initializeApplauseData
+{
+    for (int i = 0; i < [levelConfigList count]; i++)
+    {
+        [self initializeApplauseData:i];
+    }
+}
+
+- (void) initializeApplauseData:(int)level
+{
+    NSMutableDictionary *config = [levelConfigList objectAtIndex:level];
+    NSString *stream_url = [NSString stringWithFormat:@"https://api.soundcloud.com/tracks/%@/stream?client_id=%@", [config objectForKey:@"trackId"], @"fa1fd2df5a17a560f8456aed4016160a"];
+    
+    [SCRequest performMethod:SCRequestMethodGET
+                  onResource:[NSURL URLWithString:stream_url]
+             usingParameters:nil
+                 withAccount:nil
+      sendingProgressHandler:nil
+             responseHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+                 applause1Data = data;
+                 NSLog(@"Applause Data LOADED");
+             }];
 }
 
 @end
